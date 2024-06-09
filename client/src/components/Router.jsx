e6e18cc1668b2122d0b72594bae277ecf3b8fcb7
Shaunak-Kayande <shaunakk.2801@gmail.com>
2024-06-09 22:14:16 +0530

    Added React project, created routes

---
+++ client/src/components/Router.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+const Router = () => {
+  return (
+    <div>
+      <Routes>
+        {/* Home route */}
+        <Route path="/" element={"Login page"} />
+
+        {/* Customer side routes */}
+        <Route path="customer">
+          <Route path="" element={"Customer Home"} />
+          <Route path="listing" element={"Listing of products or services"} />
+          {/* Dynamic routes */}
+          <Route path=":business">
+            <Route path="" element={"Business home"} />
+            <Route path="service/:service" element={"Service home"} />
+            <Route path="product/:product" element={"Product home"} />
+          </Route>
+          <Route path="cart" element={"Customer Cart"} />
+          <Route path="checkout" element={"Customer Checkout"} />
+        </Route>
+
+        {/* Business side rutes */}
+        <Route path="business">
+          <Route path="signup" element={"Business signup"} />
+          <Route path="orders" element={"Business Orders"} />
+          <Route path="products" element={"Business Products List"} />
+          <Route path="services" element={"Business Services List"} />
+          <Route path="add-product" element={"Business add product page"} />
+          <Route path="add-service" element={"Business add service page"} />
+          <Route path="payments" element={"Business payments page"} />
+          <Route path="profile" element={"Business profile page"} />
+          <Route path="feedback" element={"Business feedback page"} />
+        </Route>
+      </Routes>
+    </div>
+  );
+};
+
+export default Router;
